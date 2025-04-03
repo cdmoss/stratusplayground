6955ed0ec23fe359a54f5d3b7bbf8dcc264cb4a2
unknown <chase@revivewellness.ca>
2025-04-02 21:50:39 -0600

    added deletion and fixed styling

--- customergen/views.py
+++ customergen/views.py
@@ -7,8 +7,24 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def generate_customer_view(request):
+    if request.method == 'POST':
+        prompt = request.POST.get('prompt') 
+        print(prompt)
+        new_customer_intel = LlmBot.process(prompt=prompt, intel_class=CustomerIntel)
+        Customer.objects.create(
+            first_name=new_customer_intel.first_name,
+            last_name=new_customer_intel.last_name,
+            email=new_customer_intel.email,
+            phone=new_customer_intel.phone,
+            address=new_customer_intel.address,
+            city=new_customer_intel.city,
+            state=new_customer_intel.state,
+            zip_code=new_customer_intel.zip_code,
+            country=new_customer_intel.country
+        )
+    
     customers = Customer.objects.all()
-    glue_query_set(request, unique_name='customers', target=customers)
+    glue_query_set(request, unique_name='customers', target=customers, access='delete')
 
     return TemplateResponse(request, 'index.html', context={})
 
@@ -18,7 +34,7 @@ def edit_customer(request):
         customer_id = request.GET.get('customer_id')
         print(customer_id)
         customer = Customer.objects.get(id=customer_id)
-        glue_model_object(request, 'customer', customer, access='change')
+        glue_model_object(request, 'customer', customer, access='delete')
         return TemplateResponse(request, 'customer_edit.html', {'customer': customer})
     
     if request.method == 'POST':
@@ -37,7 +53,7 @@ def edit_customer(request):
             country=new_customer_intel.country
         )
 
-        glue_model_object(request, 'customer', new_customer, access='change')
+        glue_model_object(request, 'customer', new_customer, access='delete')
 
         return TemplateResponse(request, 'customer_edit.html', {'customer': new_customer})
     else:
